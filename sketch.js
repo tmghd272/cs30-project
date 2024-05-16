abdc99028681b1a292042d4b23e1fbd1cde2eba9
Ninja <44890370+tmghd272@users.noreply.github.com>
2024-05-16 12:14:12 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,20 +18,58 @@
 // }
 
 let player, floor;
+let bricks, tilesGroup;
 
 function setup() {
 	new Canvas(windowWidth, windowHeight);
 	world.gravity.y = 10;
 	player = new Sprite(50, 0);
-	floor = new Sprite(250, 500, 'Infinity', 40, 'static');
+	floor = new Sprite(250, 500, 1000, 40, 'static');
+
+	bricks = new Group();
+	bricks.w = 20;
+	bricks.h = 10;
+	bricks.tile = '=';
+
+	tilesGroup = new Tiles(
+		[
+			'=====...======...===........====...................',
+			'======..======...===..............................',
+			'==..==..==.......===..............................',
+			'==..==..==.......===..............................',
+			'======..=====....===....========...................',
+			'=====...======...===..............................',
+			'==..........==...===..............................',
+			'==..........==...===..............................',
+			'==......======...==========.......................',
+			'==......=====....==========........................'
+		],
+		100,
+		40,
+		bricks.w + 4,
+		bricks.h + 4
+	);
 }
 
 function draw() {
 	clear();
-	if (mouse.presses()) {
-		player.vel.y = -4;
+	if (key === 'a') {
+		player.vel.y = -3;
+		player.vel.x = -3;
+	} else if (key === 'd') {
+		player.vel.y = -7;
 		player.vel.x = 3;
 	}
+	
+
 
 	camera.x = player.x;
+
+	clear();
+
+	for (let brick of bricks) {
+		if (brick.mouse.hovers()) {
+			brick.color = '#AA4A44';
+		}
+	}
 }
